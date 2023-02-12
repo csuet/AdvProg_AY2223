607bb0dcb9333b5b40de744b5600a4f1add2debe
Le Vu Viet Anh <vietanhlevu2004@gmail.com>
2023-02-12 15:21:32 +0700

    Modify hangman.cpp #2 (fix length - line 88)

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -86,7 +86,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 ***/
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
-    string secretWord(answerWord.length,'-');
+    string secretWord(answerWord.length(),'-');
     return secretWord;
 }
 
