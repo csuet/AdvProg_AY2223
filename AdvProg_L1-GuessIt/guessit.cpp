cb470f1c7c603d19a19204f0dda1d87597b81ba8
hoangquocvietuet@gmail.com <hoangquocvietuet@gmail.com>
2023-02-12 16:47:12 +0700

    update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <random>
+#include <chrono>
 #include "guessit.h"
 
 using namespace std;
@@ -14,7 +16,8 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    mt19937_64 rng(chrono::system_clock::now().time_since_epoch().count());
+    return std::uniform_int_distribution<long long>(1, 100)(rng);
 }
 
 
@@ -26,8 +29,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int guess;
+    cout << "Enter Your guess: \n";
+    cin >> guess;
+    return guess;
 }
 
 
@@ -46,7 +51,13 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+    if (number == randomNumber) {
+        answer = "Congratulation! You win.";
+    } else if (number > randomNumber) {
+        answer = "Your number is higher.";
+    } else {
+        answer = "Your number is lower.";
+    }
     return answer;
 }
 
@@ -59,8 +70,10 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if (answer == "Congratulation! You win.") {
+        return true;
+    }
+    return false;
 }
 
 
@@ -72,9 +85,10 @@ bool checkSuccess(string answer) {
 ***/
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
-    bool result = false;
-
-    return result;
+    if (isContinued == 'Y') {
+        return true;
+    }
+    return false;
 }
 
 
@@ -87,7 +101,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cout << "Do you want to continue playing? Enter: Y/N\n";
+    cin >> isContinued;
     return isContinued;
 }
 
@@ -113,3 +128,8 @@ int run() {
     } while (checkContinuePlaying(isContinued));
     return 0;
 }
+
+// int main(int argc, char* argv[]) {
+//     run();
+//     return 0;
+// }
