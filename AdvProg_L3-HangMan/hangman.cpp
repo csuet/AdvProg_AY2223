cf6c6e025d9cf3ef7d74e40d5394b3fc8436f540
Le Vu Viet Anh <vietanhlevu2004@gmail.com>
2023-02-12 16:26:33 +0700

    Modify hangman.cpp #11

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -115,11 +115,14 @@ char getInputCharacter() {
 void updateSecretWord(string& secretWord, const char ch, const string& word)
 {
     // TODO: Update the secret word if the character ch is in the answer word.
-    int index = word.find(ch);
     int limit = word.length();
+    do{
+    int old_index = 0;
+    int index = word.find(ch,old_index);
     if(index>=0 && index < limit){
         secretWord[index] = word[index];
     }
+    }while(index>=0 && index< limit);
 }
 
 /***
