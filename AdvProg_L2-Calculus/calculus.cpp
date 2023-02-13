336bc527963064f861c0d125772186a329fa3d66
AnNgoexe <124763946+AnNgoexe@users.noreply.github.com>
2023-02-13 22:52:13 +0700

    Update calculus.cpp

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -7,50 +7,25 @@ using std::cout;
 using std::endl;
 using std::stod;
 
-
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
-#include <iostream>
-#include <string>
-#include <cmath>
-
-using std::string;
-using std::cout;
-using std::endl;
-using std::stod;
-
-
-double mySin(double x);
-double myCos(double x);
-double mySqrt(double x);
-
-
-double myCos(double x)
+double myCos(double x) 
 {
-    return 0.0;
-    return cos(x) ;
+    return cos(x);
 }
-
 
 double mySin(double x)
 {
-    return 0.0;
-    return sin(x) ;
+    return sin(x);
 }
 
-
-double mySqrt(double x)
-{
-    if (x < 0)
-    {
+double mySqrt(double x) {
+    if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-    else
-    {
-        return sqrt(x) ;
-    }
+    else return sqrt(x);
     return 0;
 }
