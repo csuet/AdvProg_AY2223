417558fe16df6437875dabae64a9bc76ed822193
Tran-Duy-K67JUET <tmd.id2004@gmail.com>
2023-02-13 16:52:08 +0700

    update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -15,7 +15,7 @@ using namespace std;
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
     srand(time(0));
-    return rand() % 100;
+    return rand() % 100 + 1;
 }
 
 
@@ -28,8 +28,8 @@ int generateRandomNumber() {
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
     
-    int number; cin >> number;
-
+    int number; 
+    cin >> number;
     return number;
 }
 
@@ -69,8 +69,12 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if (answer == "Congratulation! You win.") 
+    {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
