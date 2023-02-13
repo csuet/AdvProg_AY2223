fd8fb01dbf717018da209843960e99c46ebac598
GlowCheese <22021115@vnu.edu.vn>
2023-02-13 19:58:41 +0700

    GuessIt: Modify player choices
    If player choose to continue playing, he should input 'Y' instead of '1'.

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -76,7 +76,7 @@ bool checkSuccess(string answer) {
 ***/
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
-    bool result = isContinued == '1';
+    bool result = isContinued == 'Y';
 
     return result;
 }
