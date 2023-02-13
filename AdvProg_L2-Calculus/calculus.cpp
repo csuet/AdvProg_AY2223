d2c645159062f3e827e48ffe5e8de5867177550d
babifish77 <babifish77@github.com>
2023-02-13 16:30:39 +0700

    Calculus

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -18,9 +18,19 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
+double myCos(double x)
 {
-    return 0.0;
+    double c = 0, c_old=2;
+    double temp = 1;
+    int n = 1;
+    while (abs(c-c_old)>0.00001)
+    {
+        c_old = c;
+        c = c+temp;
+        temp = -x*x*temp/(n*(n+1));
+        n=n+2;
+    }
+    return c;
 }
 
 /***
@@ -31,7 +41,17 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    double s = 0, s_old=2;
+    double temp = x;
+    int n = 2;
+    while (abs(s-s_old)>0.00001)
+    {
+        s_old = s;
+        s = s+temp;
+        temp = -x*x*temp/(n*(n+1));
+        n=n+2;
+    }
+    return s;
 }
 
 
@@ -46,7 +66,12 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-    
+    if (x>0)
+	{
+        double y=1.0;
+        while (y-x/y>0.000001||x/y-y>0.000001)
+            y=(y+x/y)/2;
+        return y;
+	}
     return 0;
 }
