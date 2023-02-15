e8cebdf2fc0c51fc6c7006b9876a3878001dc46c
trungnotchung <trung09072004@gmail.com>
2023-02-15 08:47:47 +0700

    update hangman

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -112,9 +112,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
     for(int i=0; i<(int)word.length(); ++i)
     {
         if(word[i] == ch && secretWord[i] == '-')
-        {
             secretWord[i] = word[i];
-        }
     }
 }
 
@@ -128,6 +126,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
     chars += ch;
+    chars += ' ';
 }
 
 /***
