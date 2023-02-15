78425088e9f3d51abf56d90cad1b24c94a0e98ee
SangTin <sang080304@gmail.com>
2023-02-15 08:40:36 +0700

    Test

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -117,6 +117,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
     chars.append(1, ch);
+    chars.append(" ");
 }
 
 /***
@@ -163,4 +164,3 @@ void processData(const char ch, const string& word,
     updateEnteredChars(ch, incorrectChars);
    }
 }
-
