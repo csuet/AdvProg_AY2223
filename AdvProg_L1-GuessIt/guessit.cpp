5ef9bc43e2ba8bd6f663c009db4003f8a8b95e4e
minhquy <pmquy3012@gmail.com>
2023-02-15 21:51:32 +0700

    commit l1

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,8 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    srand(time(0));
+    return 1 + rand()%100;
 }
 
 
@@ -26,8 +27,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int n;
+    cout << "guess?: ";
+    cin >> n;
+    return n;
 }
 
 
@@ -38,6 +41,7 @@ int getPlayerGuess() {
     Returns:
         answer (string) : answer of computer after checking result
 ***/
+
 string getAnswer(int number, int randomNumber) {
     /*** 
         TODO: check number with randomNumber and return the result.
@@ -46,7 +50,9 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+    if(number > randomNumber) answer = "You number is higher.";
+    else if (number < randomNumber) answer = "You number is lower.";
+    else answer = "Congratulation! You win.";
     return answer;
 }
 
@@ -59,8 +65,8 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if(answer == "Congratulation! You win.") return true;
+    return false;
 }
 
 
@@ -73,7 +79,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if(isContinued != '=') result = true;
     return result;
 }
 
@@ -87,7 +93,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cin >> isContinued;
     return isContinued;
 }
 
