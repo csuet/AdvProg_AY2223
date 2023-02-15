724bbd5d338ad0673519ec346afb8a9fe9be4cf2
SangTin <sang080304@gmail.com>
2023-02-15 07:45:04 +0700

    L1

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -88,7 +88,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if (isContinued == 'y') result = true;
+    if (isContinued == '0') result = true;
     return result;
 }
 
@@ -103,7 +103,6 @@ char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
     cin >> isContinued;
-    isContinued = tolower(isContinued);
     return isContinued;
 }
 
