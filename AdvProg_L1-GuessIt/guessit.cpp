16289927d8da98a389985c73cca8e039a1f49c7a
22021196_DoTrongBinh <22021196@vnu.edu.vn>
2023-02-17 16:13:40 +0700

    New

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,7 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    return rand() % 100 + 1;
 }
 
 
@@ -26,8 +26,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int player_number;
+    cout << "Your Guess: ";
+    cin >> player_number;
+    return player_number;
 }
 
 
@@ -46,6 +48,15 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
+    if (number > randomNumber) {
+        answer = "Your number is higher.";
+    }
+    else if (number == randomNumber) {
+        answer = "Congratulation! You win.";
+    }
+    else {
+        answer = "Your number is lower.";
+    }
 
     return answer;
 }
@@ -59,8 +70,8 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if (answer == "Congratulation! You win.")    return true;
+    return false;
 }
 
 
@@ -73,7 +84,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if (isContinued == 'Y' || isContinued == 'y')    result = true;
     return result;
 }
 
@@ -87,7 +98,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cout << "Do you want to play again? (y (yes) or n (no) ) ";
+    cin >> isContinued;
     return isContinued;
 }
 
@@ -112,4 +124,8 @@ int run() {
         isContinued = getPlayerOpinion();
     } while (checkContinuePlaying(isContinued));
     return 0;
+}
+
+int main() {
+    run();
 }
