892812b0c9906c3aea8f8df6d4db9580ca69ff23
Skyaducc <skyaduc@gmail.com>
2023-02-17 09:31:37 +0700

    3

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -68,9 +68,7 @@ bool isCharInWord(const char ch, const string& word)
 string chooseWordFromList(const vector<string>& wordList, int index) 
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
-    string answer;
-    answer = wordList[index+1];
-    return answer;
+    return wordList[index];
 }
 
 /***
