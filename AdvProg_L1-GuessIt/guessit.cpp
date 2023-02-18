7ceb85136a3e8d0884fc96f3f69fcd2cf874e145
Pham Anh Quan <phamquan2112004@gmail.com>
2023-02-18 12:28:14 +0700

    Add GuessIt

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -8,26 +8,29 @@ using namespace std;
 
 /***
     Args:
-        
+
     Returns:
         number (int) : random number in range 1-100
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+
+    return (int) rand()%100;
 }
 
 
 /***
     Args:
-        
+
     Returns:
         number (int) : the number that player guessed
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
 
-    return 1;
+    int guess;
+    cin >> guess;
+    return guess;
 }
 
 
@@ -39,13 +42,21 @@ int getPlayerGuess() {
         answer (string) : answer of computer after checking result
 ***/
 string getAnswer(int number, int randomNumber) {
-    /*** 
+    /***
         TODO: check number with randomNumber and return the result.
               If number is higher than randomNumber, the answer is "Your number is higher."
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
+
+    if( number < randomNumber )
+        answer = "Your number is lower.";
+    else
+        if( number > randomNumber )
+            answer = "Your number is higher.";
+        else
+            answer = "Congratulation! You win.";
 
     return answer;
 }
@@ -59,7 +70,10 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
+
+    if(answer != "Congratulation! You win.")
+        return false;
+
     return true;
 }
 
@@ -74,19 +88,24 @@ bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
 
+    if(isContinued == 'Y')
+        result = true;
+
     return result;
 }
 
 
 /***
     Args:
-        
+
     Returns:
         isContinues (char) : player's choice (continue playing or not)
 ***/
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
+
+    cin >> isContinued;
 
     return isContinued;
 }
@@ -96,7 +115,7 @@ void playGuessIt() {
     int randomNumber = generateRandomNumber();
     int number;
     string answer;
-    
+
     do {
         number = getPlayerGuess();
         answer = getAnswer(number, randomNumber);
