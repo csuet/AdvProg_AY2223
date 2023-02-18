ba6ee8677cfcb706b5fb938a1838bb61a1de29e4
22021195BangVanChien <chienlgend24032004@gmail.com>
2023-02-18 09:29:27 +0700

    [Calculate]

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -1,52 +1,38 @@
 #include <iostream>
-#include <string>
-#include <cmath>
 
-using std::string;
-using std::cout;
-using std::endl;
-using std::stod;
+using namespace std;
 
-
-double mySin(double x);
-double myCos(double x);
-double mySqrt(double x);
-
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: cosine of x
-***/
-double myCos(double x) 
+int main()
 {
-    return 0.0;
-}
+    int num1, num2;
+    char op;
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: sine of x
-***/
-double mySin(double x)
-{
-    return 0.0;
-}
-
-
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: square root of x
-***/
-double mySqrt(double x) {
-    if (x < 0) {
-        cout << "Invalid argument" << endl;
-        exit(1);
+    cin >> num1 >> op >> num2;
+    switch (op)
+    {
+    case '+':
+        cout << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 == 0)
+            cout << "Invalid divisor" << endl;
+        else
+            cout << num1 / num2 << endl;
+        break;
+    case '%':
+        if (num2 == 0)
+            cout << "Invalid divisor" << endl;
+        else
+            cout << num1 % num2 << endl;
+        break;
+    default:
+        cout << "Invalid operator" << endl;
     }
-
-    
     return 0;
 }
