113954ddc9ce904d6d1aa21a7dcab2134f8799fb
loilon504 <loilinh504@gmail.com>
2023-02-21 10:16:36 +0700

    QuanCute v2

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -67,7 +67,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer = wordList[index];
     for (int i = 0; i < answer.length(); i++)
-        if ('a' < answer[i] || answer[i] > 'z') answer[i] = char(answer[i] - 'A' + 'a');
+        if ('a' > answer[i] || answer[i] > 'z') answer[i] = char(answer[i] - 'A' + 'a');
     return answer;
 }
 
