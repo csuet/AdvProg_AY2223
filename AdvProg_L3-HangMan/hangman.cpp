dee12d49289cb2024e3b71b0f8db9da9155acee3
ThanhHigh <ducthanhtre2004@gmail.com>
2023-02-23 16:26:34 +0700

    HomeWork L3

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -85,13 +85,13 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 
     for (int i = 0; i < lgnth; i++)
         if (temp[i] >= 'A' && temp[i] <= 'Z') 
-            answer[i] = temp[i] + ('a' - 'A');
+            answer[i] = temp[i] + int('a' - 'A');
         else 
             if (temp[i] >= 'a' && temp[i] <= 'z') 
                 answer[i] = temp[i];
 
     answer += '\0';
-    
+
     return answer;
 }
 
