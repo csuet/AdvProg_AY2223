60fdbb4ac9f582c8bbc25ece627b27ad6a18f181
22026502NguyenKhanhHuyen <125527500+22026502NguyenKhanhHuyen@users.noreply.github.com>
2023-02-24 19:48:49 +0700

    Update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -48,9 +48,9 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-    if ( number < randomNumber) answer = "Your number is lower."
-    else if ( number > randomNumber) answer = "Your number if higher."
-    else answer = "Congratulation! You win."
+    if (number < randomNumber) answer = "Your number is lower.";
+    else if (number > randomNumber) answer = "Your number if higher.";
+    else answer = "Congratulation! You win.";
     return answer;
 }
 
@@ -78,7 +78,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if (isContinued == "Yes") result = true;
+    if (isContinued == 'continue') result = true;
     return result;
 }
 
@@ -92,7 +92,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-    cout << "Will you continue playing?(Yes or No)";
+    cout << "Will you continue playing?(continue playing or not)";
     cin >> isContinued;
     return isContinued;
 }
