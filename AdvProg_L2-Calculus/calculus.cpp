9289cde589444698e3d10e0b748bfd8844044ba5
22021192-NguyenNgocTung <125429268+22021192-NguyenNgocTung@users.noreply.github.com>
2023-02-25 10:46:51 +0700

    cal

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -2,11 +2,11 @@
 #include <string>
 #include <cmath>
 #include <cstdlib>
-using namespace std ;
-// using std::string;
-// using std::cout;
-// using std::endl;
-// using std::stod;
+
+ using std::string;
+ using std::cout;
+using std::endl;
+using std::stod;
 
 
 double mySin(double x);
@@ -49,73 +49,5 @@ double mySqrt(double x) {
     }
     return sqrt(x);
 }
-double hamso(string op , double number){
-    if (op == "sin"){
-        return mySin(number) ;
-    }
-    else {
-        if ( op == "cos"){
-            return myCos(number) ;
-        }
-        else {
-            if (op = "sqrt"){
-                return mySqrt(number) ;
-            }
-            else {
-                cout << "Invalid operator." << endl ;
-                exit(1) ;
-            }
-        }
-    }
-}
-// double cal(double num1 , double num2 , char op){
-//     switch (op) {
-//         case '+' : {
-//             return num1 + num2 ;
-//         }
-//         case '-' : {
-//             return num1 - num 2 ; 
-//         }
-//         case '*' : {
-//             return num1 * num 2 ; 
-//         }
-//         case '/' : {
-//             if ( num2 == 0) {
-//                 cout << "Invalid divisor" <<endl ;
-//                 exit(1) ;
-//             }
-//             else {
-//                 return num1 / num 2 ;
-//             }
-//         }
-//         // case '%' : {
-//         //     if ( num2 == 0) {
-//         //         cout << "Invalid divisor" <<endl ;
-//         //         exit(1) ;
-//         //     }
-//         //     else {
-//         //         return num1 % num 2 ;
-//         //     }
-//         // }
-//         default : {
-//             cout << "Invalib operator" << endl ;
-//             exit(1) ;
-//         }
-//     }
-// }
-int main(int argc ,char* argv[]){
-    //if ( argc == 3){
-        string op = argv[1] ;
-        double number = atoi(argv[2]) ;
-        cout << hamso(op, number) ;
-        return 0 ;
-    //}
-    // else {
-    //     double num1 , num2 ;
-    //     char op ; 
-    //     num1 = atoi(argv[1]) ;
-    //     op = argv[2][0] ;
-    //     num2 = atoi(argv[3]) ;
-    //     cout << cal(num1 , num2 , op) << endl ;
-    // }
-}
+
+
