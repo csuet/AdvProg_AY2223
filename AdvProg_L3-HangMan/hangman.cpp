dd37d05b43cb26dcace0a7cc731401186b933580
22026526-san <126303739+22026526-san@users.noreply.github.com>
2023-02-25 16:38:30 +0700

    Update hangman.cpp

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -156,7 +156,7 @@ void processData(const char ch, const string& word,
             update incorrectGuess: call updateIncorrectGuess() function
             update incorrectChars: call updateEnteredChars() function
     ***/
-    if(isCharInWord(ch)==true){
+    if(isCharInWord(ch,word)==true){
         updateSecretWord(secretWord,ch,word);
         updateEnteredChars(ch,correctChars);
     }
