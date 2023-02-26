b849bfcf3ffa3a64010f729b32c50a25bf01e238
quangcaa <nguyenvietquang.1601@gmail.com>
2023-02-27 00:06:09 +0700

    cm4

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -49,7 +49,13 @@ bool isCharInWord(const char ch, const string& word)
 
 string chooseWordFromList(const vector<string>& wordList, int index) 
 {
-    return wordList[index] ;
+    string answer = wordList[index] ;
+    
+    for(int i=0 ; i<answer.length() ; i++)
+    {
+        answer[i] = tolower(answer[i]) ;
+    }
+    return answer ;
 }
 
 string generateHiddenCharacters(string answerWord){
