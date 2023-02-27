4cc4959771450756ec02b0731a8140db274eaf40
Trần Đình Tuấn <125422941+playboizzzzzz@users.noreply.github.com>
2023-02-27 07:20:09 +0700

    hangman.cpp

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -51,7 +51,7 @@ vector<string> readWordListFromFile(const string& filePath)
 bool isCharInWord(const char ch, const string& word)
 {
     // TODO: return true if ch is in word else return false
-    return true;
+    return false;
 }
 
 /***
