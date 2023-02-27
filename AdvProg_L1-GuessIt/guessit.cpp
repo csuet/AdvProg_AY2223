c6b8e2fb716e209b54cedbe089d89ee9a163b5d1
vuniem131104 <124224840+vuniem131104@users.noreply.github.com>
2023-02-27 20:05:49 +0700

    Update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -1,57 +1,163 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+ 
+@vuniem131104 
+wizardap
+/
+AdvProg_AY2223
+Public
+forked from csuet/AdvProg_AY2223
+Fork your own copy of wizardap/AdvProg_AY2223
+Code
+Pull requests
+Actions
+Projects
+Security
+Insights
+AdvProg_AY2223/AdvProg_L1-GuessIt/guessit.cpp
+@wizardap
+wizardap Well done
+Latest commit f60c7a3 3 weeks ago
+ History
+ 2 contributors
+@wizardap@csuet
+121 lines (99 sloc)  2.79 KB
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include "guessit.h"
 
 using namespace std;
 
-int generateRandomNumber();
-int getPlayerGuess();
-void printAnswer(int number, int randomNumber);
-void playGuessIt();
 
-int main()
-{
-    srand(time(0));
-    char isContinued;
-    do {
-        playGuessIt();
-        cout << endl << "Do you want to play again (y/n) ?  ";
-        cin >> isContinued;
-    } while (isContinued == 'y' || isContinued == 'Y');
-    return 0;
+/***
+    Args:
+    Returns:
+        number (int) : random number in range 1-100
+***/
+int generateRandomNumber() {
+    return 1 + rand() % (100);
 }
 
-int generateRandomNumber()
-{
-    return rand() % 100 + 1;
-}
 
-int getPlayerGuess()
-{
+/***
+    Args:
+    Returns:
+        number (int) : the number that player guessed
+***/
+int getPlayerGuess() {
+    // TODO: Ask the player guest and return the player's number
     int number;
-    cout << endl << "Enter your number between 1 and 100: ";
+    cout << "Enter your guess(1..100):";
     cin >> number;
     return number;
 }
 
-void printAnswer(int number, int randomNumber)
-{
-    if (number > randomNumber) {
-        cout <<"Your number is higher." << endl;
-    } else if (number < randomNumber) {
-        cout <<"Your number is lower." << endl;
-    } else {
-        cout << "Congratulation! You win." << endl;
-    }
+
+/***
+    Args:
+        number (int): answer number from player
+        randomNumber (int): the true number
+    Returns:
+        answer (string) : answer of computer after checking result
+***/
+string getAnswer(int number, int randomNumber) {
+    /***
+        TODO: check number with randomNumber and return the result.
+              If number is higher than randomNumber, the answer is "Your number is higher."
+              If number is lower than randomNumber, the answer is "Your number is lower."
+              If number is equal randomNumber, the answer is "Congratulation! You win."
+    ***/
+    string answer;
+    if (number > randomNumber) answer = "Your number is higher.";
+    else if (number < randomNumber) answer = "Your number is lower.";
+    else answer = "Congratulation! You win.";
+    return answer;
 }
 
-void playGuessIt()
-{
+
+/***
+    Args:
+        answer (string): answer from computer after compare numbers
+    Returns:
+        result (bool) : player win or not
+***/
+bool checkSuccess(string answer) {
+    // TODO: return the result after checking that player guessed right or wrong
+    if (answer == "Congratulation! You win.") return true;
+    else return false;
+}
+
+
+
+/***
+    Args:
+        isContinued (char): player's choice
+    Returns:
+        result (bool) : continue playing or not
+***/
+bool checkContinuePlaying(char isContinued) {
+    // TODO: return result after checking player continue playing or not
+    bool result = false;
+    if (isContinued == 'Y' || isContinued=='y') result=true;
+    return result;
+}
+
+
+/***
+    Args:
+    Returns:
+        isContinues (char) : player's choice (continue playing or not)
+***/
+char getPlayerOpinion() {
+    // TODO: Ask the player about continue playing and return the player's choice
+    char isContinued;
+    cout << "Do you wanna continue this game ? (Y/N) : ";
+    cin >> isContinued;
+    
+    return isContinued;
+}
+
+
+void playGuessIt() {
     int randomNumber = generateRandomNumber();
     int number;
+    string answer;
 
     do {
         number = getPlayerGuess();
-        printAnswer(number, randomNumber);
-    } while (number != randomNumber);
+        answer = getAnswer(number, randomNumber);
+        cout << answer << endl;
+    } while (!checkSuccess(answer));
 }
+
+int run() {
+    srand(time(0));
+    char isContinued;
+    do {
+        playGuessIt();
+        isContinued = getPlayerOpinion();
+    } while (checkContinuePlaying(isContinued));
+    return 0;
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+AdvProg_AY2223/guessit.cpp at master · wizardap/AdvProg_AY2223
