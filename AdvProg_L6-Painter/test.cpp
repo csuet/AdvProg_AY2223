1a6ae82bf8b3ae37e1037585e44c26a569eb151d
RainbowBunny <vmc15566@gmail.com>
2023-02-28 08:30:33 +0700

    ??

--- AdvProg_L6-Painter/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    double ang = 60;
-    int numpixels = 30;
-    cout << numpixels * cos(ang / 180 * M_PI) << ' ' << numpixels * sin(ang / 180 * M_PI) << endl;
-}
