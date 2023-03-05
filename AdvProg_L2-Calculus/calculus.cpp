d7dd9695980c5d40706b4f3adead77e43b0ad528
LyTranHuy <115777706+LyTranHuy@users.noreply.github.com>
2023-03-05 21:18:46 +0700

    Update calculus.cpp

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -12,29 +12,57 @@ double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: cosine of x
-***/
-double myCos(double x) 
-{
-    return 0.0;
+double trituyetdoi(double n) {
+	if (n < 0) 
+		return -n;
+	else 
+		return n;
+}
+double giaithua(double x) {
+	if (x == 0) {
+		return 1;
+	}
+	else {
+		return x * giaithua(x - 1);
+	}
 }
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: sine of x
-***/
-double mySin(double x)
-{
-    return 0.0;
+double mu(double x, int n) {
+	double result = 1;
+	for (int i = 1; i <= n; i++) {
+		result *= x;
+	}
+	return result;
 }
 
-
+double myCos(double x) {
+	const double EPSILON = 0.001; // maximum difference between the expected result and returned result
+	const double PI_TEST = 3.14159265359;
+	double result = 0;
+	double term = 1;
+	double n = x * PI_TEST / 180;
+	int i = 0;
+	while (trituyetdoi(term) >= EPSILON) {
+		term = (mu(-1, i) * mu(n, 2 * i)) / giaithua(2 * i);
+		result += term;
+		i++;
+	}
+	return result;
+}
+double mySin(double x) {
+	const double EPSILON = 0.001; // maximum difference between the expected result and returned result
+	const double PI_TEST = 3.14159265359;
+	double result = 0;
+	double term = 1;
+	double n = x * PI_TEST / 180;
+	int i = 0;
+	while (trituyetdoi(term) >= EPSILON) {
+		term = (mu(-1, i) * mu(n, 2 * i + 1)) / giaithua(2 * i + 1);
+		result += term;
+		i++;
+	}
+	return result;
+}
 /***
     Args:
         x (double): a number
@@ -42,11 +70,16 @@ double mySin(double x)
         double: square root of x
 ***/
 double mySqrt(double x) {
-    if (x < 0) {
-        cout << "Invalid argument" << endl;
-        exit(1);
-    }
-
-    
-    return 0;
+	const double EPSILON = 0.001; // maximum difference between the expected result and returned result
+	if (x < 0) {
+		return NULL;
+	}
+	if (x == 0) {
+		return 0;
+	}
+	double guess = x / 2;
+	while (trituyetdoi(guess * guess - x) > EPSILON) {
+		guess = (guess + x / guess) / 2;
+	}
+	return guess;
 }
