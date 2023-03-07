72dc8863e68298882c7c9242cf64434b5a183897
Thân Việt Anh 22026503 <124753939+TVAexe@users.noreply.github.com>
2023-03-07 20:45:02 +0700

    Update calculus.cpp

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
-#include <string>
-#include <cmath>
+#include <bits/stdc++.h>
+
 
 using std::string;
 using std::cout;
@@ -20,7 +19,7 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
+    return cos(x);
 }
 
 /***
@@ -31,7 +30,7 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    return sin(x)
 }
 
 
@@ -46,7 +45,7 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
+    else return sqrt(x)
     
     return 0;
 }
