0e863645a73bde0bc697c1c9f3b00689e0df7a08
Sleeplessmen <khaibk.a.1109@gmail.com>
2023-03-07 20:37:50 +0700

    changeshavemade

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -12,41 +12,21 @@ double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: cosine of x
-***/
 double myCos(double x) 
 {
-    return 0.0;
+    double cos(x);
 }
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: sine of x
-***/
 double mySin(double x)
 {
-    return 0.0;
+    return sin(x);
 }
 
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: square root of x
-***/
 double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-    
-    return 0;
+    double sqrt(x);
 }
