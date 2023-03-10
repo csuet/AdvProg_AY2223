24aafd3105f272fa5193f8b459be470e08cca63c
TranVuDucHuy <22021111@vnu.edu.vn>
2023-03-10 11:07:27 +0700

    Huy feel so sad

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -137,6 +137,8 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
     chars.push_back(ch);
+    char c = ' ';
+    chars.push_back(c);
 }
 
 /***
