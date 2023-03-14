c82878004167f5827385cf6baa7d48060fd4537b
quan29117 <dangminhquan29117@gmail.com>
2023-03-14 07:47:32 +0700

    Modify snake

--- AdvProg_L7-Snake/Game.cpp
+++ AdvProg_L7-Snake/Game.cpp
@@ -202,7 +202,7 @@ void Game::setCellType(Position pos, CellType cellType)
 	//
 	// START CODE HERE
 	if (pos.isInsideBox(0, 0, width, height))
-		squares[pos.x][pos.y] = cellType;
+		squares[pos.y][pos.x] = cellType;
 	// END CODE HERE
 }
 
